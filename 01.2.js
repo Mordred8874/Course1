bbb16bb075262591fe34477f876428c170e1605f
mordred8874 <antonbest1998@gmail.com>
2021-12-17 04:16:28 -0800

    2

---
+++ 01.2.js
@@ -0,0 +1,37 @@
+// "use strict";
+
+// if (4 == 9) {
+//     console.log('ok');
+// } else {
+//     console.log('Error');
+// }
+
+
+// // if (num < 49) {
+// //     console.log('Error');
+// // } else if (num>100){
+// //     console.log('too much');
+// // }else {
+// //     console.log('Ok!');
+// // }
+
+// // (num === 50) ? console.log('Ok!') : console.log('Error');
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//         console.log('NEVERNO');
+//         break;
+//     case 100:
+//         console.log('NEVERNO');
+//         break;
+//     case 50:
+//         console.log('VERNO');
+//         break;
+//     default:
+//         console.log('not this time');
+//         break;
+// }
+
+
--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const numberOfFilms = +prompt('Сколько фильмов вы ужепосмотрели?','');
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы ужепосмотрели?', '');
 
 const personalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors :{},
-    genres :[],
-    privat : false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const a = prompt('Один из последнихпросмотренных фильмов?',''),
- b = prompt('На сколько оцениваете его?',''),
- c = prompt('Один из последнихпросмотренных фильмов?',''),
- d = prompt('На сколько оцениваете его?','');
 
- personalMovieDB.movies[a] = b;
- personalMovieDB.movies[c] = d;
+for(let i = 0;i < 2;i++){
+    const a = prompt('Один из последнихпросмотренных фильмов?', ''),
+          b = prompt('На сколько оцениваете его?', '');
+         
+    if (a != null && b != null && a != '' && b != '' && a.length<50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+
+     personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count<10){
+    console.log('Просмотрено мало фильмов');
+} else if (personalMovieDB.count>=10 && personalMovieDB.count<30){
+    console.log('Вы классический кинозритель');
+}else if (personalMovieDB.count>=30){
+    console.log('Вы киноман');
+}else {
+    console.log('Error');
+}
 
 console.log(personalMovieDB);
+
+
+
